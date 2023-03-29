90e4615136b87b6f64457882da27066de3aa1ae6
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-29 22:17:37 +0530

    The Programme file

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StackAndQueueProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the stack element");
+            UC_1CreateStack stacklist = new UC_1CreateStack();
+            stacklist.Push(70);
+            stacklist.Push(30);
+            stacklist.Push(56);
+            stacklist.Display();
+            Console.ReadLine();
+        }
+    }
+}
