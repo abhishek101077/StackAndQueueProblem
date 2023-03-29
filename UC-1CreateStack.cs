fd67c984a183f038c8e14e6640b9715408a3780b
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-29 22:22:23 +0530

    The stack problem

---
+++ UC-1CreateStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StackAndQueueProblem
+{
+     class UC_1CreateStack
+    {
+        private Node top;
+
+        public void LinkedListStack()
+        {
+            this.top = null;
+        }
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed in the stack value", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
